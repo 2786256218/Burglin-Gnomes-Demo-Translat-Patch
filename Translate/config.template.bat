516889367a7948a16f3a2547b71e9044a25089be
ASwave-Code storm ZP <2786256218@qq.com>
2026-04-17 13:00:38 +0800

    Delete Translate directory

--- Translate/config.template.bat
+++
@@ -1,8 +0,0 @@
-:: Configuration file for Gnomium Translation Build
-:: Set the path to your game installation directory here.
-:: For example: set GAME_DIR=C:\Program Files (x86)\Steam\steamapps\common\Burglin' Gnomes Demo
-set GAME_DIR=..
-
-:: Set the path to MSBuild/csc directory if you have a custom installation.
-:: For example: set MSBUILD_DIR=E:\Data\MSBuild
-set MSBUILD_DIR=
